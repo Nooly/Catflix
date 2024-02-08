918529438c760d93e72372e6bcd48cb09908cff9
Nooly <NoolyTheCat@gmail.com>
2024-02-08 13:59:13 +0200

    added .env to gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,6 @@
-// Import the Express.js module for creating web applications
 import express from "express";
-
-// Import the CORS (Cross-Origin Resource Sharing) module to allow requests from different origins
 import cors from "cors";
-
-// Import the Mongoose module for MongoDB object modeling
 import mongoose from "mongoose";
-
-// Import the dotenv module to load environment variables from a .env file into process.env
 import dotenv from "dotenv";
 import seedRouter from "./routes/seedRouter.js";
 
@@ -26,10 +19,8 @@ app.use(express.json());
 // Add a middleware to the Express application that parses incoming requests with URL-encoded payloads
 app.use(express.urlencoded({ extended: false }));
 
-// Set the port for the server to listen on, either from the environment variable PORT or default to 8080
 const PORT = process.env.PORT || 8080;
 
-// Use the seedRouter for routes starting with "/api/v1/seed"
 app.use("/api/v1/seed", seedRouter);
 
 // Middleware for handling errors
